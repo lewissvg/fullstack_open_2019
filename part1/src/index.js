05ec0634767e5dd7f3ca922b7882e02f419e9947
lewisbrotchie <lewis@yulife.com>
2019-08-29 10:49:37 +0100

    part 1 exercise 1.5 complete

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Header = props => <h1>{props.course}</h1>;
+const Header = props => <h1>{props.course.name}</h1>;
 
 const Part = props => (
   <p>
@@ -10,6 +10,7 @@ const Part = props => (
 );
 
 const Content = props => {
+  console.log(props);
   return (
     <>
       <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
@@ -28,27 +29,29 @@ const Total = props => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
     </div>
   );
 };
