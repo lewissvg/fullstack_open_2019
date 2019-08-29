a405da93faa905e11f97c4a0a94dec2bccb032ce
lewisbrotchie <lewis@yulife.com>
2019-08-29 14:40:34 +0100

    unicafe step 1 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Button = props => <button>{props.text}</button>;
+const Header = props => <h1>{props.text}</h1>;
+
+const Button = props => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
   // save clicks of each button to own state
@@ -9,7 +13,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <Button text="good" />;
+  const clickHandler = (name, newValue) => {
+    if (name === "good") {
+      setGood(newValue);
+    } else if (name === "neutral") {
+      setNeutral(newValue);
+    } else {
+      setBad(newValue);
+    }
+  };
+
+  return (
+    <>
+      <Header text="give feedback" />
+      <Button handleClick={() => clickHandler("good", good + 1)} text="good" />
+      <Button
+        handleClick={() => clickHandler("neutral", neutral + 1)}
+        text="neutral"
+      />
+      <Button handleClick={() => clickHandler("bad", bad + 1)} text="bad" />
+      <Header text="statistics" />
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+      </p>
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
