f74c441fd5633578b089e98d17f7109efd960e29
lewisbrotchie <lewis@yulife.com>
2019-08-30 14:36:08 +0100

    anecdotes step3 completed

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Header = props => <h1>{props.text}</h1>;
+
+const Button = props => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = props => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(
@@ -26,14 +32,24 @@ const App = props => {
     setVotes(copy);
   };
 
+  const findHighest = () => {
+    return votes.indexOf(Math.max.apply(Math, votes));
+  };
+
   return (
     <>
+      <Header text="Anecdote of the day" />
       <div>{props.anecdotes[selected]}</div>
       has {votes[selected]} votes
       <div>
-        <button onClick={() => handleVote()}>vote</button>
-        <button onClick={() => handleNext()}>next anecdote</button>
+        <Button handleClick={() => handleVote()} text="vote" />
+        <Button handleClick={() => handleNext()} text="next anecdote" />
       </div>
+      <div>
+        <Header text="Anecdote with most votes" />
+      </div>
+      <div>{props.anecdotes[findHighest()]}</div>
+      has {votes[findHighest()]} votes
     </>
   );
 };
